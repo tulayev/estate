9fd0aa9be0393ab2f238e6907d51b5820d3a1b41
Yan Kuzin <iankuzin@gmail.com>
2025-06-10 10:45:33 +0500

    system tag policy edited

--- app/MoonShine/Resources/TagResource.php
+++ app/MoonShine/Resources/TagResource.php
@@ -13,6 +13,7 @@ use MoonShine\Decorations\Block;
 use MoonShine\Fields\ID;
 use MoonShine\Fields\Text;
 use MoonShine\Fields\Color;
+use App\Helpers\Constants;
 
 /**
  * @extends ModelResource<Tag>
@@ -34,7 +35,12 @@ class TagResource extends ModelResource
             Block::make([
                 ID::make()->sortable(),
 
-                Text::make(__('Moonshine/Tags/TagResource.name'), 'name')->required(),
+                Text::make(__('Moonshine/Tags/TagResource.name'), 'name')
+                    ->required()
+                    ->disabled(function($item = null) {
+                        return $item && $item->id && in_array($item->id, array_values(Constants::SYSTEM_TAG_IDS));
+                    })
+                    ->hint(__('Moonshine/Tags/TagResource.system_tag_hint')),
 
                 Color::make(__('Moonshine/Tags/TagResource.color_ui_tag'), 'color_ui_tag')
                     ->nullable()
@@ -47,14 +53,54 @@ class TagResource extends ModelResource
 
     public function rules(Model $item): array
     {
-        return [
-            'name' => 'required|string|max:255',
+        $rules = [
             'color_ui_tag' => 'nullable|string|max:50',
         ];
+        
+        // For system tags, add a custom validation rule for the name field
+        // that ensures the name hasn't changed from its original value
+        if ($item->exists && in_array($item->id, array_values(Constants::SYSTEM_TAG_IDS))) {
+            $originalTag = Tag::find($item->id);
+            
+            $rules['name'] = [
+                'required',
+                'string',
+                'max:255',
+                function ($attribute, $value, $fail) use ($originalTag) {
+                    if ($value !== $originalTag->name) {
+                        $fail('The name of system tags cannot be changed.');
+                    }
+                }
+            ];
+        } else {
+            $rules['name'] = 'required|string|max:255';
+        }
+        
+        return $rules;
     }
 
     public function redirectAfterSave(): string
     {
         return url('/admin/resource/tag-resource/index-page');
+    }
+    
+    public function beforeSave(Model $item): Model
+    {
+        // If this is an update to a system tag, preserve the original name
+        if ($item->exists && in_array($item->id, array_values(Constants::SYSTEM_TAG_IDS))) {
+            // Force-reload the original tag from the database (without any cached values)
+            $originalTag = Tag::where('id', $item->id)->first();
+            if ($originalTag) {
+                // Reset the name back to the original, ensuring it can't be changed
+                $item->name = $originalTag->name;
+                
+                // If using translatable package, make sure to preserve all translations too
+                if (method_exists($item, 'getTranslations')) {
+                    $item->setTranslations('name', $originalTag->getTranslations('name'));
+                }
+            }
+        }
+        
+        return $item;
     }
 }
--- lang/en/Moonshine/Tags/TagResource.php
+++ lang/en/Moonshine/Tags/TagResource.php
@@ -4,6 +4,7 @@ return [
     'name' => 'Name',
     'color_ui_tag' => 'Color',
     'color_hint'=> 'Choose a color for this tag to be displayed',
+    'system_tag_hint' => 'System tag - name cannot be edited',
 ];
 
 
