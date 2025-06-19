fd1c2714de3a35e159d514340eede2ea984898eb
Yan Kuzin <iankuzin@gmail.com>
2025-06-19 19:21:33 +0500

    gradient on card listings list view

--- resources/views/components/pages/listing/index/list-view/card.blade.php
+++ resources/views/components/pages/listing/index/list-view/card.blade.php
@@ -52,7 +52,7 @@
                         loading="lazy"
                     />
                 </div>
-                <div class="absolute border-rounded inset-0 bg-gradient-50"></div>
+                <div class="absolute border-rounded inset-0 bg-gradient-to-b from-transparent to-black/35"></div>
                 <!-- Image Top -->
                 <div class="relative z-10">
                     <!-- Tags (Left side) -->
