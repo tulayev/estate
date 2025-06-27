ce3ffb487310dc3d867d820c1a3d8987d240833b
Umarbek Tulaev <69952016+tulayev@users.noreply.github.com>
2025-06-27 23:59:14 +0500

    fix: developer section img styles

--- resources/views/components/pages/listing/show/history.blade.php
+++ resources/views/components/pages/listing/show/history.blade.php
@@ -11,7 +11,7 @@
             >
                 <div class="shadow-card bg-white border-rounded">
                     <div class="flex flex-col lg:flex-row items-center relative">
-                        <div class="w-full lg:w-2/5 h-auto lg:h-[250px] flex items-center justify-center">
+                        <div class="lg:w-[250px] lg:h-[250px] flex items-center justify-center flex-shrink-0 overflow-hidden">
 {{--                            <img--}}
 {{--                                src="{{ asset('assets/images/three-stars.png') }}"--}}
 {{--                                alt="Three Stars"--}}
@@ -21,18 +21,18 @@
                                 <img
                                     src="{{ Helper::resolveImagePath($hotel->topic->logo) }}"
                                     alt="{{ $hotel->topic->title }}"
-                                    class="border-rounded w-full h-full object-contain"
+                                    class="lg:w-[230px] lg:h-[230px] object-cover aspect-square border-rounded"
                                 />
                             @endif
                         </div>
-                        <div class="w-full lg:w-3/5 h-auto lg:h-[250px]">
+                        <div class="lg:h-[250px]">
                             <div class="p-4 text-primary">
-                                <h3 class="text-lg sm:text-xl font-bold group-hover:text-secondary">
+                                <h3 class="text-xl md:text-2xl font-bold md:font-black group-hover:text-secondary">
                                     {{ $hotel->topic->title }}
                                 </h3>
 
                                 <p class="mt-4 text-sm sm:text-base font-semibold group-hover:text-secondary">
-                                    {{ Str::limit(Helper::removeHtmlTags($hotel->topic->body), 200) }}
+                                    {{ Str::limit(Helper::removeHtmlTags($hotel->topic->body), 400) }}
                                 </p>
                             </div>
                         </div>
